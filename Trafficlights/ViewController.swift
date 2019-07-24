456555d91677f1f1e06a1f61d16f3a3f71329616
Burenkovuser <nb2207@yandex.ru>
2019-07-24 11:35:10 +0700

    worked

--- Trafficlights/ViewController.swift
+++ Trafficlights/ViewController.swift
@@ -10,11 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var redlightView: UIView!
+    @IBOutlet weak var yellowlightView: UIView!
+    @IBOutlet weak var greenlightView: UIView!
+    
+    var lighVavue = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 10
+        
+        redlightView.layer.cornerRadius = redlightView.layer.bounds.size.height / 3.1415
+        yellowlightView.layer.cornerRadius = yellowlightView.layer.visibleRect.size.height / 3.1415
+        greenlightView.layer.cornerRadius = greenlightView.layer.visibleRect.size.height / 3.1415
+        
+        redlightView.backgroundColor = UIColor.red
+        yellowlightView.backgroundColor = UIColor.yellow
+        greenlightView.backgroundColor = UIColor.green
+        
+        redlightView.alpha = 0.3
+        yellowlightView.alpha = 0.3
+        greenlightView.alpha = 0.3
+        
     }
 
 
+    @IBAction func startButtonAction(_ sender: Any) {
+        
+        startButton.setTitle("NEXT", for: .normal)
+        
+        lighVavue = lighVavue == 3 ? 1 : lighVavue + 1
+        
+        redlightView.alpha = 0.3
+        yellowlightView.alpha = 0.3
+        redlightView.alpha = 0.3
+        
+        switch lighVavue {
+        case 1:
+            redlightView.alpha = 1
+        case 2:
+            yellowlightView.alpha = 1
+        case 3:
+            greenlightView.alpha = 1
+        default:
+            break
+        }
+        
+    }
 }
 
